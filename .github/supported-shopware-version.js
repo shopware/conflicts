ca55ab1a28c2c25b04e85ba26ed2d34b419374b8
Soner Sayakci <s.sayakci@gmail.com>
2025-05-07 13:19:57 +0200

    feat: exclude known broken Shopware versions from supported versions list

--- .github/supported-shopware-version.js
+++ .github/supported-shopware-version.js
@@ -4,9 +4,13 @@ const response = await fetch('https://repo.packagist.org/p2/shopware/core.json')
 const json = await response.json();
 
 const versions = [];
+const knownBrokenVersions = ['v6.6.10.1', 'v6.6.10.0'];
 
 for (const version of json.packages['shopware/core']) {
     if (version.version.startsWith('v6.5') || version.version.startsWith('v6.6') || version.version.startsWith('v6.7')) {
+        if (knownBrokenVersions.includes(version.version)) {
+            continue;
+        }
         versions.push(version.version);
     }
 }
