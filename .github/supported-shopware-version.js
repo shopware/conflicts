5150a5ae5deb956f115f992869f723460e0fe963
Soner Sayakci <s.sayakci@gmail.com>
2025-05-07 12:24:28 +0200

    ci: add nightly install workflow with dynamic Shopware version matrix

---
+++ .github/supported-shopware-version.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const response = await fetch('https://repo.packagist.org/p2/shopware/core.json');
+const json = await response.json();
+
+const versions = [];
+
+for (const version of json.packages['shopware/core']) {
+    if (version.version.startsWith('v6.5') || version.version.startsWith('v6.6') || version.version.startsWith('v6.7')) {
+        versions.push(version.version);
+    }
+}
+
+console.log(JSON.stringify({
+    matrix: {
+        include: versions.map(version => ({
+            version
+        }))
+    }
+}))
