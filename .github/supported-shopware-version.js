7a4bb4a98d25f776a1b2b9148f8d4c48ea3305ac
Soner Sayakci <s.sayakci@gmail.com>
2025-05-07 13:20:16 +0200

    chore: add comment explaining broken versions in Symfony bug list

--- .github/supported-shopware-version.js
+++ .github/supported-shopware-version.js
@@ -4,7 +4,9 @@ const response = await fetch('https://repo.packagist.org/p2/shopware/core.json')
 const json = await response.json();
 
 const versions = [];
-const knownBrokenVersions = ['v6.6.10.1', 'v6.6.10.0'];
+const knownBrokenVersions = [
+    'v6.6.10.1', 'v6.6.10.0' // Symfony Bug, has been fixed in 6.6.10.2
+];
 
 for (const version of json.packages['shopware/core']) {
     if (version.version.startsWith('v6.5') || version.version.startsWith('v6.6') || version.version.startsWith('v6.7')) {
